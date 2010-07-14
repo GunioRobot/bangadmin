527324860ae2f70751e8c7d844d7d66d99e510dc
Sudheer Satyanarauana <sudheer.s@sudheer.net>
2010-07-15 01:35:36 +0530

    centos after install -y

--- linux/centos/base/after-fresh-install.sh
+++ linux/centos/base/after-fresh-install.sh
@@ -1,18 +1,53 @@
+#!/bin/sh
+
+#  Copyright (c) 2010, Sudheera Satyanarayana.
+#  All rights reserved.
+# 
+#    Redistribution and use in source and binary forms, with or without modification,
+#    are permitted provided that the following conditions are met:
+# 
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+# 
+#    * Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation
+#      and/or other materials provided with the distribution.
+# 
+#    * Neither the names of Sudheera Satyanarayana nor the names of the project
+#      contributors may be used to endorse or promote products derived from this
+#      software without specific prior written permission.
+# 
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#  
+# 
+
+
 # Things to do after fresh install of CentOS
 
-# update
-yum update
+# update everything
+yum update -y
 
 # Install vim-enhanced
 yum install vim-enhanced
 # Add alias vi=vim in /root/.bashrc
 echo "alias vi='vim'"  > ~/.bashrc
 
-# Configure repositories
-# Install essential software
-yum install htop
-
-
 # Install development packages
-yum install gcc kernel-devel kernel-headers
+yum install gcc kernel-devel kernel-headers sqlite-devel sqlite -y
+
+# Configure repositories
+
+# Install server administration essential software
+yum install htop -y
+
+
 
